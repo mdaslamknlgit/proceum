0fee1dfe111ede4770e75646958b484bfe379699
hareesh.enugu <hareesh.enugu@sparshcom.net>
2021-03-05 10:47:26 +0530

    login front end validations

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -9,14 +9,21 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 	currentYear: number = new Date().getFullYear();
 	login = {email:'', password: ''};
+	public message:String = 'Invalid email or password';
 	constructor(private http: AuthService, private route: Router) { }
 	ngOnInit(): void {}
   	doLogin(){
-    	let params = {url: 'login', email:'hareesh@sparshcom.net', password: '12345678'};
-    	this.http.login(params).subscribe((res) =>{
-      		sessionStorage.setItem("_token", res["token"]);
-			  this.route.navigate(['/dashboard']);
-    	});
+    	let params = {url: 'login', email: this.login.email, password: this.login.password};
+    	this.http.login(params).subscribe((res) =>{console.log(res);
+			if(res['error']){
+				this.login.password = '';
+				this.message = res['message'];
+			}
+			else{
+				sessionStorage.setItem("_token", res["token"]);
+			  	this.route.navigate(['/dashboard']);
+			}
+      	});
   	}
 	logout(){
 		let params = {url: 'logout', email:'hareesh@sparshcom.net', password: '12345678'};
