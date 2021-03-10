2d4953967bbc76aae0fb70438d6e2d73ee577e98
hareesh.enugu <hareesh.enugu@sparshcom.net>
2021-03-10 12:08:32 +0530

    main student admin navigations added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,46 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules  } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { AuthGuardService as AuthGuard } from './auth/auth-guard.service';
 const routes: Routes = [
-  	{ path: '', pathMatch: 'full', redirectTo: 'login' },
-  	{
-		path: 'login',
-		loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
-  	},
-  	{
-		path: 'dashboard',
-		loadChildren: () => import('./admin/dashboard/dashboard.module').then(m => m.DashboardModule),
-        canActivate: [AuthGuard]
-  	},
-  	{ path: '**', redirectTo: 'login' }
+  { path: '', pathMatch: 'full', redirectTo: 'main' },
+  {
+    path: 'main',
+    loadChildren: () =>
+      import('./frontend/frontend.module').then((m) => m.FrontendModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'admin/dashboard',
+    loadChildren: () =>
+      import('./admin/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+    canActivate: [AuthGuard],
+    data: {
+      role: '1',
+    },
+  },
+  {
+    path: 'student',
+    loadChildren: () =>
+      import('./frontend/frontend.module').then((m) => m.FrontendModule),
+    canActivate: [AuthGuard],
+    data: {
+      role: '2',
+    },
+  },
+  { path: '**', redirectTo: 'login' },
 ];
 @NgModule({
-  	imports: [RouterModule.forRoot(routes, { useHash: true, preloadingStrategy: PreloadAllModules })],
-  	exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true,
+      preloadingStrategy: PreloadAllModules,
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/frontend/frontend.module.ts
+++ src/app/frontend/frontend.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
-
+import { RouterModule, Routes } from '@angular/router';
+import { MaterialModule } from '../material/material.module';
+import { IndexComponent } from './index/index.component';
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: '',
+    component: IndexComponent,
+  },
+];
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [CommonModule],
+  declarations: [DashboardComponent, IndexComponent],
+  imports: [CommonModule, RouterModule.forChild(routes), MaterialModule],
+  exports: [RouterModule],
 })
 export class FrontendModule {}
