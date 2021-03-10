0dfb325a5b4fc18f8b6050e6522cddbe4cd4aa24
hareesh.enugu <hareesh.enugu@sparshcom.net>
2021-03-10 12:36:49 +0530

    bug fix

--- src/app/frontend/frontend.module.ts
+++ src/app/frontend/frontend.module.ts
@@ -4,6 +4,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
 import { IndexComponent } from './index/index.component';
+import { LayoutsModule } from '../layouts/layouts.module';
 const routes: Routes = [
   {
     path: 'dashboard',
@@ -16,7 +17,12 @@ const routes: Routes = [
 ];
 @NgModule({
   declarations: [DashboardComponent, IndexComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), MaterialModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MaterialModule,
+    LayoutsModule,
+  ],
   exports: [RouterModule],
 })
 export class FrontendModule {}
