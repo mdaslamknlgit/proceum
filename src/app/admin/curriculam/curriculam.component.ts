684d26cc86ea7d17b296e2d80cf9395dccd496bd
hareesh.enugu <hareesh.enugu@sparshcom.net>
2021-03-24 14:51:07 +0530

    menu issue fix

--- src/app/admin/curriculam/curriculam.component.ts
+++ src/app/admin/curriculam/curriculam.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CommonService } from 'src/app/services/common.service';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
+import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-curriculam',
   templateUrl: './curriculam.component.html',
-  styleUrls: ['./curriculam.component.scss']
+  styleUrls: ['./curriculam.component.scss'],
 })
 export class CurriculamComponent implements OnInit {
-
-  constructor() { }
-
+  public template = {
+    id: '',
+    template: '',
+    template_subject: '',
+    template_name: '',
+  };
+  displayedColumns: string[] = [
+    'id',
+    'subject',
+    'status',
+    'created_at',
+    'updated_at',
+    'actions',
+  ];
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  public page = 0;
+  public model_status = false;
+  constructor(private http: CommonService, public dialog: MatDialog) {}
   ngOnInit(): void {
+    this.getTemplates();
   }
-
+  toggleModel() {
+    this.model_status = !this.model_status;
+  }
+  getTemplates() {
+    let param = { url: 'email-template' };
+    this.http.get(param).subscribe((res) => {
+      this.dataSource = new MatTableDataSource(res['data']['templates']);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
+  }
+  public getServerData(event?: PageEvent) {}
+  public doFilter = (value: string) => {
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+  };
+  manageTemplate(id) {}
+}
+export interface TemplateResponce {
+  s_no: number;
+  id: number;
+  subject: string;
+  status: number;
+  created_at: number;
+  updated_at: string;
 }
