837a8fb9f3c09645df7f6b6cc580089a556336fd
JDMAC <jdmac@JDMACs-MacBook-Pro.local>
2021-03-25 21:22:04 +0530

    design changes

--- src/app/frontend/frontend.module.ts
+++ src/app/frontend/frontend.module.ts
@@ -14,6 +14,7 @@ import { MyAccountComponent } from './student/my-account/my-account.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { EmailVerifiedComponent } from './email-verified/email-verified.component';
 import { UnsubscribeEmailComponent } from './unsubscribe-email/unsubscribe-email.component';
+
 const routes: Routes = [
   {
     path: 'dashboard',
