83fab891af2e77f775f3370ee9d875e6a9127684
Narayana Udayagiri <narayana.udayagiri@sparshcom.net>
2021-04-05 19:14:07 +0530

    counties listing and create country form validations completed

--- src/app/admin/countries-states-cities/countries-states-cities.component.ts
+++ src/app/admin/countries-states-cities/countries-states-cities.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CommonService } from '../../services/common.service';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
 
 export interface PeriodicElement {
   name: string;
@@ -7,33 +12,32 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-countries-states-cities',
   templateUrl: './countries-states-cities.component.html',
   styleUrls: ['./countries-states-cities.component.scss']
 })
 
-
-
 export class CountriesStatesCitiesComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-  constructor() { }
+  displayedColumns: string[] = ['s_no', 'country_name', 'state_name', 'city_name', 'action'];
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  public num_rows: number = 0;
+  public page = 0;
+  public pageSize = 10;
+  public sort_by: any;
+  constructor(private http:CommonService) { }
 
   ngOnInit(): void {
+    let params={
+      url: 'get-countries-states-cities'
+    };
+    this.http.get(params).subscribe((res: Response) => {
+      this.dataSource = new MatTableDataSource(res['data']['countries']);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+    });
   }
   
   public model_status = false;
@@ -42,3 +46,11 @@ export class CountriesStatesCitiesComponent implements OnInit {
   }
 
 }
+
+export interface Response {
+  error: boolean;
+  message: string;
+  errors?: any;
+
+}
+
