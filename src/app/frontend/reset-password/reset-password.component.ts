d323ea566589e1e751e7bd9db8ab13ed507aa342
Narayana Udayagiri <narayana.udayagiri@sparshcom.net>
2021-04-05 15:54:16 +0530

    issues fixed and reset password functionality completed

---
+++ src/app/frontend/reset-password/reset-password.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router,ActivatedRoute  } from '@angular/router';
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: './reset-password.component.html',
+  styleUrls: ['./reset-password.component.scss']
+})
+export class ResetPasswordComponent implements OnInit {
+  password_hide: boolean = true;
+  password_confirm_hide: boolean = true;
+  password_check: boolean = false;
+  confirm_check: boolean = false;
+  public message: string = '';
+  is_show: boolean = false;
+  reset: Reset = { new_password: '', confirm_password: '' };
+  hash_token:any;
+  constructor(private http: AuthService,private toastr: ToastrService,private router: Router,private activatedRoute: ActivatedRoute) {
+    this.hash_token = this.activatedRoute.snapshot.paramMap.get('token');
+   }
+
+  ngOnInit(): void {
+  }
+
+  doResetPassword(){
+
+    if(this.reset.new_password == '' || this.reset.confirm_password == ''){
+      this.message = "Required data is missing";
+      this.toastr.error(this.message, 'Error', { closeButton: true , timeOut: 5000});
+      return;
+    }else{
+
+      if(this.reset.new_password != ""){
+        const regex = new RegExp('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&#^()><?/:;,.])[A-Za-z\d$@$!%*?&#^()><?/:;,.].{7,15}');
+        this.password_check = regex.test(this.reset.new_password);
+        if(this.password_check == true){
+          if(this.reset.new_password === this.reset.confirm_password){
+            this.confirm_check = true;
+            let params = {
+              url: 'reset-password',
+              password:this.reset.new_password,
+              hash_token:this.hash_token
+            };
+            this.http.register(params).subscribe((res: Response) => {
+              if(res.error) {
+                this.is_show = false;
+                this.toastr.error(res.message, 'Error', { closeButton: true , timeOut: 5000});
+              }else{
+                this.is_show = true;
+                this.toastr.success(res.message, 'Success', { closeButton: true, timeOut: 5000 });
+              }
+            });
+
+          }else{
+            this.confirm_check = false;
+            this.message = "Password and Confirm password are not matched";
+            this.toastr.error(this.message , 'Error', { closeButton: true , timeOut: 5000});
+            
+            return;
+          }
+        }else{
+          this.message = "A minimum 8 characters password contains a combination of uppercase and lowercase letter,special character and number are required.";
+          this.toastr.error(this.message, 'Error', { closeButton: true , timeOut: 5000});
+          return;
+        }
+
+  
+      }
+
+    }
+
+  }
+
+  passwordFun() {
+    this.password_hide = !this.password_hide;
+  }
+  passwordConfirmFun(){
+    this.password_confirm_hide = !this.password_confirm_hide;
+  }
+
+}
+
+export interface Reset {
+  new_password: any;
+  confirm_password: any;
+}
+
+export interface Response {
+  error: boolean;
+  message: string;
+  errors?: any;
+}
