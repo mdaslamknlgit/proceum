4d32f7a49babab3deb4e6d4af0678a54278defbe
Prashanth Kumar Kunta <Prashanth.kunta@sparshcom.net>
2021-04-09 10:46:11 +0530

    comit changes of newsletter

--- src/app/admin/settings/settings.component.ts
+++ src/app/admin/settings/settings.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from '../../services/common.service';
 import { ToastrService } from 'ngx-toastr';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { AuthService } from '../../auth/auth.service';
 @Component({
   selector: 'app-settings',
@@ -10,151 +10,159 @@ import { AuthService } from '../../auth/auth.service';
 })
 export class SettingsComponent implements OnInit {
 
-  filedata:any;
-  imgMsg:any;
-  imgMessage:any;
-  gstin_check:boolean=true;
-  email_check:boolean=true;
-  address_check:boolean=true;
-  imagePath:any;
-  mode_check:boolean=true;
-  color:string;
+  filedata: any;
+  imgMsg: any;
+  imgMessage: any;
+  gstin_check: boolean = true;
+  email_check: boolean = true;
+  address_check: boolean = true;
+  imagePath: any;
+  mode_check: boolean = true;
+  color: string;
   touchUi;
-  url:any="./assets/images/ProceumLogo.png";
-  src:any;
-  systemMode:any;
-  settings:any={
-    "organization_name":"",
-    "contact_name":"",
-    "gstin_number":"",
-    "contact_number_1":"",
-    "contact_number_2":"",
-    "contact_email_1":"",
-    "contact_email_2":"",
-    "full_address":"",
-    "date_format":"",
-    "time_format":"",
-    "list_view_limit":"",
-    "theme_color":"",
+  url: any = "./assets/images/ProceumLogo.png";
+  src: any;
+  public model_status = false;
+  systemMode: any;
+  settings: any = {
+    "organization_name": "",
+    "contact_name": "",
+    "gstin_number": "",
+    "contact_number_1": "",
+    "contact_number_2": "",
+    "contact_email_1": "",
+    "contact_email_2": "",
+    "full_address": "",
+    "date_format": "",
+    "time_format": "",
+    "list_view_limit": "",
+    "theme_color": "",
   }
   constructor(
-    private http: CommonService,private toaster: ToastrService,
-    private http1:  AuthService
-    ) {}
+    private http: CommonService, private toaster: ToastrService,
+    private http1: AuthService
+  ) { }
 
   ngOnInit(): void {
-    this.src=this.url;
+    this.src = this.url;
     this.getSystemMode();
+    this.systemMode = 'live';
   }
-  fileEvent(e){
-    this.filedata = e.target.files;   
+  fileEvent(e) {
+    this.filedata = e.target.files;
     const mimeType = this.filedata[0].type;
     if (mimeType.match(/image\/*/) == null) {
-        this.imgMessage = "Only images are supported like jpg,png,jpeg.";
-        return;
-    }else{
+      this.imgMessage = "Only images are supported like jpg,png,jpeg.";
+      return;
+    } else {
       this.imgMessage = "";
-      this.src = this.url; 
+      this.src = this.url;
     }
     const reader = new FileReader();
     this.imagePath = this.filedata;
-    reader.readAsDataURL(this.filedata[0]); 
-    reader.onload = (_event) => { 
-        this.src = reader.result; 
+    reader.readAsDataURL(this.filedata[0]);
+    reader.onload = (_event) => {
+      this.src = reader.result;
     }
   }
 
-  getSystemMode(){
-    let params={
-      "url":'get-mode'
+  getSystemMode() {
+    let params = {
+      "url": 'get-mode'
     }
     this.http.post(params).subscribe((res) => {
       console.log(res['sys_mode'].value);
-      this.systemMode=res['sys_mode'].value;
-    }); 
-
+      this.systemMode = (res['sys_mode'].value) ? res['sys_mode'].value : "live";
+    });
   }
+
 
   createSettings() {
     var myFormData = new FormData();
-    if(this.settings.organization_name.trim() !==""){
+    if (this.settings.organization_name.trim() !== "") {
       myFormData.append('organization_name', this.settings.organization_name.trim());
     }
-console.log(1);
-//address validation
-    if(this.settings.full_address.trim() ===""){
-        this.address_check=false;
-        return;
+    console.log(1);
+    //address validation
+    if (this.settings.full_address.trim() === "") {
+      this.address_check = false;
+      return;
     }
     console.log(1);
-//logo
-    if(this.filedata){
+    //logo
+    if (this.filedata) {
       let mimeType = this.filedata[0].type;
       let size = this.filedata[0].size;
       if (mimeType.match(/image\/*/) == null) {
-          this.imgMessage = "Only images are supported like jpg,png,jpeg.";
-          return;
+        this.imgMessage = "Only images are supported like jpg,png,jpeg.";
+        return;
       }
       console.log(1);
-      
-      if(size >= 204800){
+
+      if (size >= 204800) {
         this.imgMessage = "logo must be less than 200kb";
         return;
       }
-        this.imgMessage = "";
-        myFormData.append('logo', this.filedata[0]);            
-  }
+      this.imgMessage = "";
+      myFormData.append('logo', this.filedata[0]);
+    }
     myFormData.append('contact_email_1', this.settings.contact_email_1.trim());
     myFormData.append('contact_email_2', this.settings.contact_email_2.trim());
     myFormData.append('contact_number_1', this.settings.contact_number_1.trim());
     myFormData.append('contact_number_2', this.settings.contact_number_2.trim());
     myFormData.append('full_address', this.settings.full_address.trim());
     myFormData.append('gstin_number', this.settings.gstin_number.trim());
-    myFormData.append('contact_name', this.settings.contact_name.trim());    
-    myFormData.append('date_format', this.settings.date_format.trim());    
-    myFormData.append('time_format', this.settings.time_format.trim());    
-    myFormData.append('list_view_limit', this.settings.list_view_limit);    
-    myFormData.append('theme_color', this.settings.theme_color);    
-    myFormData.append('copyright_text', this.settings.copy_right);  
+    myFormData.append('contact_name', this.settings.contact_name.trim());
+    myFormData.append('date_format', this.settings.date_format.trim());
+    myFormData.append('time_format', this.settings.time_format.trim());
+    myFormData.append('list_view_limit', this.settings.list_view_limit);
+    myFormData.append('theme_color', this.settings.theme_color);
+    myFormData.append('copyright_text', this.settings.copy_right);
 
-    let param={
-      'url':'settings'      
+    let param = {
+      'url': 'settings'
     };
-    this.http.formData(param,myFormData).subscribe((res) => {
-      if(res['error'] == false){
+    this.http.formData(param, myFormData).subscribe((res) => {
+      if (res['error'] == false) {
         this.toaster.success(res['message'], 'Success', {
           progressBar: true,
         });
         (<HTMLFormElement>document.getElementById('settings_form')).reset();
-      }else{
+      } else {
         this.toaster.error(res['message'], 'Error', { progressBar: true });
       }
-    }); 
+    });
+  }
+  
+  toggleModel() {
+    this.model_status = !this.model_status;
   }
 
-  updateSysMode(){
+
+  updateSysMode() {
+    this.model_status = !this.model_status;
     const user = JSON.parse(atob(sessionStorage.getItem('user')));
-    if(this.systemMode ===""){
-      this.mode_check=false;
+    if (this.systemMode === "") {
+      this.mode_check = false;
       return;
-    }else{
-      this.mode_check=true;
+    } else {
+      this.mode_check = true;
     }
-    let param={
-      'url':'update-settings-mode',
-      "mode":this.systemMode,
-      "user_id":user.id
+    let param = {
+      'url': 'update-settings-mode',
+      "mode": this.systemMode,
+      "user_id": user.id
     };
-     this.http.post(param).subscribe((res) => {
-      if(res['error'] == false){
+    this.http.post(param).subscribe((res) => {
+      if (res['error'] == false) {
         this.toaster.success(res['message'], 'Success', {
           progressBar: true,
         });
         (<HTMLFormElement>document.getElementById('settings_form')).reset();
-      }else{
+      } else {
         this.toaster.error(res['message'], 'Error', { progressBar: true });
       }
-     });
+    });
   }
 
 }
