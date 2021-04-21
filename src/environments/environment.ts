cab459df1fe4efd1b38dd86e7852d25df823921f
JDMAC <jdmac@JDMACs-MBP.domain.name>
2021-04-21 11:33:27 +0530

    design issues

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   page_size: 10,
   page_size_options: [10, 20, 50, 100],
   // apiUrl: 'http://192.168.0.8:8080/api/',
-  apiUrl: 'http://192.10.250.106:8000/api/',
-  // apiUrl  : 'https://apidev.proceum.com/api/'
+  // apiUrl: 'http://192.10.250.106:8000/api/',
+  apiUrl  : 'https://apiqa.proceum.com/api/'
 };
 
 /*
