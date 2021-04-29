cf85f3245b960c6607d6ea86e34ecac2a7988161
JDMAC <jdmac@JDMACs-MacBook-Pro.local>
2021-04-29 13:40:52 +0530

    sidemenu and spinner issue

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,5 @@
 import { Component, HostListener } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { CommonService } from '../services/common.service';
 
 @Component({
   selector: 'admin-root',
@@ -8,12 +7,6 @@ import { CommonService } from '../services/common.service';
   styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent {
-  loading: boolean;
-  constructor(private loaderService: CommonService, private router: Router) {
-    this.loaderService.isLoading.subscribe((v) => {
-      this.loading = v;
-    });
-  }
+  constructor(private router: Router) {}
   ngOnInit() {}
-
 }
