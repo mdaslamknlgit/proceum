d97d37ee36f44dbc8fa20f044fc33e226e05c7ee
hareeshreddy13 <hareesh.enugu@sparshcom.net>
2021-05-06 10:56:08 +0530

    editor plugins added

--- src/app/admin/custom-pages/create-custom-pages/create-custom-pages.component.ts
+++ src/app/admin/custom-pages/create-custom-pages/create-custom-pages.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import * as ClassicEditor from '../../../../assets/ckeditor5/build/ckeditor';
 import { CommonService } from '../../../services/common.service';
 import { ToastrService } from 'ngx-toastr';
 import { environment } from '../../../../environments/environment';
@@ -8,7 +8,7 @@ import { UploadAdapter } from '../UploadAdapter';
 @Component({
   selector: 'app-create-custom-pages',
   templateUrl: './create-custom-pages.component.html',
-  styleUrls: ['./create-custom-pages.component.scss']
+  styleUrls: ['./create-custom-pages.component.scss'],
 })
 export class CreateCustomPagesComponent implements OnInit {
   isPageRequired: boolean = true;
@@ -16,43 +16,44 @@ export class CreateCustomPagesComponent implements OnInit {
   isMenuDisplay: boolean = true;
   isPageDisplay: boolean = true;
   customPage = {
-    "old_page_name": "",
-    "new_page_name": "",
-    "new_menu_name": "",
-    "old_menu_name": "",
-    "isShowChecked": "",
-    "page_content": "",
+    old_page_name: '',
+    new_page_name: '',
+    new_menu_name: '',
+    old_menu_name: '',
+    isShowChecked: '',
+    page_content: '',
   };
   public Editor = ClassicEditor;
   menuList: any;
   pagesList: any;
-  constructor(
-    private http: CommonService, private toaster: ToastrService
-  ) { }
+  constructor(private http: CommonService, private toaster: ToastrService) {}
 
   ngOnInit(): void {
     this.getMenusAndPages();
   }
   htmlEditorConfig = {
+    toolbar: {
+      items: environment.ckeditor_toolbar,
+    },
     mediaEmbed: {
-        previewsInData: true
-    }
-}
+      previewsInData: true,
+    },
+  };
 
   getMenusAndPages() {
     let params = {
-      "url": "get-menu-pages"
+      url: 'get-menu-pages',
     };
     this.http.post(params).subscribe((res) => {
       this.menuList = res['menu'];
     });
   }
 
-  getPages(){
-    this.pagesList =[];
+  getPages() {
+    this.pagesList = [];
     let params = {
-      "url": "pages",
-      'parent_id':this.customPage.old_menu_name
+      url: 'pages',
+      parent_id: this.customPage.old_menu_name,
     };
     this.http.post(params).subscribe((res) => {
       this.pagesList = res['pages'];
@@ -60,10 +61,10 @@ export class CreateCustomPagesComponent implements OnInit {
   }
 
   selectPage() {
-    if (this.customPage.old_page_name != "") {
+    if (this.customPage.old_page_name != '') {
       this.isPageDisplay = false;
       this.isPageRequired = false;
-      this.customPage.new_page_name="";
+      this.customPage.new_page_name = '';
     } else {
       this.isPageRequired = true;
       this.isPageDisplay = true;
@@ -71,50 +72,58 @@ export class CreateCustomPagesComponent implements OnInit {
   }
 
   selectMenu() {
-    if (this.customPage.old_menu_name != "") {
+    if (this.customPage.old_menu_name != '') {
       this.isMenuDisplay = false;
       this.isMenuRequired = false;
-      this.customPage.new_menu_name="";
+      this.customPage.new_menu_name = '';
     } else {
       this.isMenuRequired = true;
       this.isMenuDisplay = true;
     }
-  this.getPages();
+    this.getPages();
   }
-  
+
   onReady(eventData) {
     let apiUrl = environment.apiUrl;
-    eventData.plugins.get('FileRepository').createUploadAdapter = function (loader) {
-        var data =new UploadAdapter(loader,apiUrl+'upload-image');
-        return data;
+    eventData.plugins.get('FileRepository').createUploadAdapter = function (
+      loader
+    ) {
+      var data = new UploadAdapter(loader, apiUrl + 'upload-image');
+      return data;
     };
   }
 
   createCustomPage() {
-    let i=0;
+    let i = 0;
     if (this.isMenuRequired) {
-      if(this.customPage.new_menu_name.trim()==""){
-        this.customPage.new_menu_name=this.customPage.new_menu_name.trim();
+      if (this.customPage.new_menu_name.trim() == '') {
+        this.customPage.new_menu_name = this.customPage.new_menu_name.trim();
         i++;
       }
     }
     if (this.isPageRequired) {
-      if(this.customPage.new_page_name.trim()==""){
-        this.customPage.new_page_name=this.customPage.new_page_name.trim();
+      if (this.customPage.new_page_name.trim() == '') {
+        this.customPage.new_page_name = this.customPage.new_page_name.trim();
         i++;
       }
     }
-    if(i!=0){
+    if (i != 0) {
       return;
     }
     let params = {
-      'url': 'create-page',
-      'page_name': (this.customPage.old_page_name) ? this.customPage.old_page_name : this.customPage.new_page_name,
-      'menu_name': (this.customPage.old_menu_name) ? this.customPage.old_menu_name : this.customPage.new_menu_name,
-      'menu_check': (this.isMenuRequired) ? false : true,
-      'page_check': (this.isPageRequired) ? true : false,
-      'page_content': this.customPage.page_content,
-      'show_menu': (this.customPage.isShowChecked) ? this.customPage.isShowChecked : false,
+      url: 'create-page',
+      page_name: this.customPage.old_page_name
+        ? this.customPage.old_page_name
+        : this.customPage.new_page_name,
+      menu_name: this.customPage.old_menu_name
+        ? this.customPage.old_menu_name
+        : this.customPage.new_menu_name,
+      menu_check: this.isMenuRequired ? false : true,
+      page_check: this.isPageRequired ? true : false,
+      page_content: this.customPage.page_content,
+      show_menu: this.customPage.isShowChecked
+        ? this.customPage.isShowChecked
+        : false,
     };
     this.http.post(params).subscribe((res) => {
       if (res['error'] == false) {
@@ -131,5 +140,3 @@ export class CreateCustomPagesComponent implements OnInit {
     });
   }
 }
-
-
