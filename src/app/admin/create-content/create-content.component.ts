fbaad32d4900367c82235d881d4edd3c792d66ea
hareeshreddy13 <hareesh.enugu@sparshcom.net>
2021-05-19 14:33:19 +0530

    attachments and image issues

--- src/app/admin/create-content/create-content.component.ts
+++ src/app/admin/create-content/create-content.component.ts
@@ -4,7 +4,7 @@ import { environment } from 'src/environments/environment';
 import * as Editor from '../../../assets/ckeditor5/build/ckeditor';
 import { UploadAdapter } from '../../classes/UploadAdapter';
 import { CommonService } from 'src/app/services/common.service';
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-create-content',
   templateUrl: './create-content.component.html',
@@ -38,6 +38,11 @@ export class CreateContentComponent implements OnInit {
     },
     language: 'en',
   };
+  public library_purpose: any;
+  public attachments = [];
+  public attachment_files = [];
+  public images = [];
+  public images_files = [];
   onReady(eventData) {
     let apiUrl = environment.apiUrl;
     eventData.plugins.get('FileRepository').createUploadAdapter = function (
@@ -47,18 +52,54 @@ export class CreateContentComponent implements OnInit {
       return data;
     };
   }
-  constructor(private http: CommonService) {}
+  constructor(private http: CommonService, private toster: ToastrService) {}
   ngOnInit(): void {
     this.http.child_data.subscribe((res) => {
-      console.log(res);
+      if (this.library_purpose == 'attachments') {
+        let obj = { file_path: res['file_path'], path: res['path'] };
+        if (this.attachment_files.includes(obj['file_path'])) {
+          this.toster.error('File is already used', 'File Exists');
+        } else {
+          this.toster.success('Files Added.', 'File');
+          this.attachments.push(obj);
+          this.attachment_files.push(obj['file_path']);
+        }
+      }
+      if (this.library_purpose == 'images') {
+        let obj = { file_path: res['file_path'], path: res['path'] };
+        if (this.images_files.includes(obj['file_path'])) {
+          this.toster.error('File is already used', 'File Exists');
+        } else {
+          this.toster.success('Files Added.', 'File');
+          this.images.push(obj);
+          this.images_files.push(obj['file_path']);
+        }
+      }
     });
   }
   CloseModal() {
     this.library_popup = false;
   }
-  openAssetsLibrary(tab) {
-    alert(tab);
+  openAssetsLibrary(tab, purpose) {
+    this.library_purpose = purpose;
     this.active_tab = tab;
     this.library_popup = true;
+  }
+  removeFile(index, purpose) {
+    if (purpose == 'attachments') {
+      const index2 = this.attachment_files.indexOf(
+        this.attachments[index]['file_path']
+      );
+      if (index2 > -1) {
+        this.attachment_files.splice(index2, 1);
+        this.attachments.splice(index, 1);
+      }
+    } else if (purpose == 'images') {
+      const index2 = this.images_files.indexOf(this.images[index]['file_path']);
+      if (index2 > -1) {
+        this.images_files.splice(index2, 1);
+        this.images.splice(index, 1);
+      }
+    }
   }
 }
