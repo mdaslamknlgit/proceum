b0b1bd22a8e3c159c11f8be590e8a949fed2787a
hareeshreddy13 <hareesh.enugu@sparshcom.net>
2021-06-04 10:48:58 +0530

    scroll to top added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,34 @@ export class AppComponent {
   }
   public is_admin: boolean = false;
   public is_student: boolean = false;
+  windowScrolled = false;
   constructor(private http: CommonService, private router: Router) {}
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if (
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop > 100
+    ) {
+      this.windowScrolled = true;
+    } else if (
+      (this.windowScrolled && window.pageYOffset) ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop < 10
+    ) {
+      this.windowScrolled = false;
+    }
+  }
+  scrollToTop() {
+    (function smoothscroll() {
+      var currentScroll =
+        document.documentElement.scrollTop || document.body.scrollTop;
+      if (currentScroll > 0) {
+        window.requestAnimationFrame(smoothscroll);
+        window.scrollTo(0, currentScroll - currentScroll / 8);
+      }
+    })();
+  }
   ngOnInit() {
     window.addEventListener('keyup', (e) => {
       var keyCode = e.keyCode ? e.keyCode : e.which;
