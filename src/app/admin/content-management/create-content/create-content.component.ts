f4126a13e44c4ed5ffaf566da2a50409ad846e64
mohanchaitanya <mohanchaitanya.k@sparshcom.net>
2021-06-14 11:46:56 +0530

    for designs - mohan

--- src/app/admin/content-management/create-content/create-content.component.ts
+++ src/app/admin/content-management/create-content/create-content.component.ts
@@ -6,12 +6,42 @@ import { UploadAdapter } from '../../../classes/UploadAdapter';
 import { CommonService } from 'src/app/services/common.service';
 import { ToastrService } from 'ngx-toastr';
 import { ActivatedRoute, Router } from '@angular/router';
+
+
+
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 @Component({
   selector: 'app-create-content',
   templateUrl: './create-content.component.html',
   styleUrls: ['./create-content.component.scss'],
 })
+
+ 
+
+
 export class CreateContentComponent implements OnInit {
+  displayedColumns: string[] = ['position', 'name'];
+  dataSource = ELEMENT_DATA;
   public active_tab = 'images';
   @ViewChild('editor', { static: false }) editor: CKEditorComponent;
   public library_popup: boolean = false;
@@ -236,3 +266,4 @@ export class CreateContentComponent implements OnInit {
     });
   }
 }
+
