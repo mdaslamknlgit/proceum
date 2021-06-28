a2b33e8794187b96dab9ef677caaa37ad4b3f0c2
hareeshreddy13 <hareesh.enugu@sparshcom.net>
2021-06-28 10:31:06 +0530

    bug fixes

--- src/app/frontend/student/levels/details/details.component.ts
+++ src/app/frontend/student/levels/details/details.component.ts
@@ -132,7 +132,7 @@ export class DetailsComponent implements OnInit, AfterViewInit {
         this.curriculum = data['curriculum'];
         this.breadcome = res['breadcome'];
         this.content_list = data['content_list'];
-        this.content = data['content'];
+        this.content = data['content'] ? data['content'] : [];
         this.highyields = data['highyields'];
         this.mcqs = data['mcqs'];
         this.short_answers = data['short_answers'];
