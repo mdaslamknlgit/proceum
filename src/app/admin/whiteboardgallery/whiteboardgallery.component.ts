7d520e595067c2f28df51c183e351efc0755f60a
phanindra.ch <phanindra.ch@sparshcom.net>
2021-07-14 11:10:10 +0530

    Added Whiteboard Gallery Code

--- src/app/admin/whiteboardgallery/whiteboardgallery.component.ts
+++ src/app/admin/whiteboardgallery/whiteboardgallery.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CommonService } from 'src/app/services/common.service';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { environment } from '../../../environments/environment';
+import { MatSort } from '@angular/material/sort';
+import { ToastrService } from 'ngx-toastr';
+import { MatDialog } from '@angular/material/dialog';
 
 export interface PeriodicElement {
   name: string;
@@ -7,18 +14,7 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
+
 
 @Component({
   selector: 'app-whiteboardgallery',
@@ -26,11 +22,140 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./whiteboardgallery.component.scss']
 })
 export class WhiteboardgalleryComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
-  constructor() { }
+  //displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['id','title','created_by','created_at','actions'];
+  dataSource = new MatTableDataSource(); 
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  public num_rows: number = 0;
+  public page = 0;
+  public pageSize = environment.page_size;
+  public page_size_options = environment.page_size_options;
+  public sort_by: any;
+  public search_txt ="";
+  public show_list_view = true;
+  public properties_popup: boolean = false;
+  public preview_path = '';
+  public title = '';
+  public tname = '';
+  public created_at = '';
+  whiteboard: any = {
+    search_text: ''
+  }
+  constructor(
+    private http: CommonService,
+    public toster: ToastrService,
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    //this.getWhiteboardGallery();
+    this.changeView('list');
+  }
+
+  changeView(parm){
+    if(parm == 'list'){
+      this.page = 0;
+      this.pageSize = environment.page_size;
+      this.show_list_view = true;
+      console.log('Page => '+this.page+', limit => '+this.pageSize)
+      let param = { url: 'get-whiteboard-gallery',"parmType":"list","offset": this.page, "limit": this.pageSize, "sort_by": this.sort_by, "search":this.whiteboard.search_text.trim()};
+      this.http.post(param).subscribe((res) => {
+        if (res['error'] == false) {
+          this.dataSource = new MatTableDataSource(res['whiteboard']);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+          this.num_rows = res['whiteboard_count'];
+        } else {
+          this.toster.info(res['message'], 'Info');
+        }
+        
+      });
+    }
+    if(parm == 'grid'){
+      this.whiteboard.search_text = '';
+      this.show_list_view = false;
+      let param = { url: 'get-whiteboard-gallery',"parmType":"gird", "search":this.search_txt};
+      this.http.post(param).subscribe((res) => {
+        if (res['error'] == false) {
+          this.dataSource = res['whiteboard'];
+        } else {
+          this.toster.info(res['message'], 'Info');
+        }
+        
+      });
+
+    }
+  }
+
+  deleteContentData(id, type) {
+    let param = {
+      url: 'whiteboard-delete/' + id ,
+      delete:1
+    };
+    this.http.post(param).subscribe((res) => {
+      if (res['error'] == false) {
+        this.toster.success(res['message'], 'Success', { closeButton: true });
+        if(type == 'list'){
+          this.changeView(type);
+        }
+        if(type == 'grid'){
+          this.changeView(type);
+        }        
+      } else {
+        this.toster.error(res['message'], res['message'], {
+          closeButton: true,
+        });
+      }
+    });
+  }
+
+  public getServerData(event?: PageEvent) {
+    this.pageSize = event.pageSize;
+		this.page = (event.pageSize * event.pageIndex);
+    this.pageFilter();
+  }
+  
+  public pageFilter(){
+    let param = { url: 'get-whiteboard-gallery',"parmType":"list","offset": this.page, "limit": this.pageSize, "search":this.whiteboard.search_text.trim()};
+    this.http.post(param).subscribe((res) => {
+      if (res['error'] == false) {
+        this.dataSource = new MatTableDataSource(res['whiteboard']);
+        this.num_rows = res['whiteboard_count'];
+      } else {
+        this.toster.info(res['message'], 'Info');
+      }
+      
+    });
+  }
+
+  public doFilter(){
+    //console.log('okkk'+this.whiteboard.search_text.trim());
+    //console.log('Page => '+this.page+', limit => '+this.pageSize)
+    this.changeView('list');
+    /*let param = { url: 'get-whiteboard-gallery',"parmType":"list","offset": this.page, "limit": this.pageSize, "search":this.whiteboard.search_text.trim()};
+    this.http.post(param).subscribe((res) => {
+      if (res['error'] == false) {
+        this.dataSource = new MatTableDataSource(res['whiteboard']);
+        //this.dataSource.paginator = this.paginator;
+        this.num_rows = res['whiteboard_count'];
+      } else {
+        this.toster.info(res['message'], 'Info');
+      }
+      
+    });*/
+  }
+
+  showPropertisModal(image_path,title,tname,created_at) {
+    this.preview_path = image_path;
+    this.title = title;
+    this.tname = tname;
+    this.created_at = created_at;
+    this.properties_popup = true;
+  }
+
+  ClosePropertisModal() {
+    this.properties_popup = false;
   }
 
 }
