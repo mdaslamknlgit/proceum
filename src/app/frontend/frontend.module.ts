5c5487c9c7d9152d4813c4b595b30828447e6b3a
hareeshreddy13 <hareesh.enugu@sparshcom.net>
2021-11-11 11:51:30 +0530

    exam mode

--- src/app/frontend/frontend.module.ts
+++ src/app/frontend/frontend.module.ts
@@ -34,10 +34,10 @@ import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { TestsPageComponent } from './student/study-planner-student/tests-page/tests-page.component';
 import { QbankTestsPageComponent } from './student/levels/qbank-tests-page/qbank-tests-page.component';
 //import { QbankComponent } from './student/levels/qbank/qbank.component';
-import { StudyModeComponent } from './student/exams/study-mode/study-mode.component';
-import { ExamModeComponent } from './student/exams/exam-mode/exam-mode.component';
+import { StudyModeComponent } from './student/levels/exams/study-mode/study-mode.component';
+import { ExamModeComponent } from './student/levels/exams/exam-mode/exam-mode.component';
 import { LiveModeComponent } from './student/exams/live-mode/live-mode.component';
-import { ExamPrepmodeComponent } from './student/exams/exam-prepmode/exam-prepmode.component';
+import { ExamPrepmodeComponent } from './student/levels/exams/exam-prepmode/exam-prepmode.component';
 
 // import { StlModelViewerModule } from 'angular-stl-model-viewer';
 
@@ -76,16 +76,14 @@ const routes: Routes = [
         path: 'curriculums/:curriculum_id/:level_id/:level_parent_id',
         component: Level_listComponent,
       },
-      {
-        path: 'qbank/:curriculum_id/:level_id/:level_parent_id',
-        component: QbankTestsPageComponent,
-      },
+      { path: 'qbank/create-exam/:qbank_id', component: ExamPrepmodeComponent },
+      { path: 'qbank/study-mode/:qbank_id/:source_id', component: StudyModeComponent },
+      { path: 'qbank/exam-mode/:qbank_id/:exam_id', component: ExamModeComponent },
+      { path: 'qbank/:curriculum_id/:level_id/:level_parent_id', component: QbankTestsPageComponent },
       { path: 'study-planner', component: StudyPlannerStudentComponent },
-      { path: 'study-mode', component: StudyModeComponent },
-      { path: 'exam-mode', component: ExamModeComponent },
+      //{ path: 'exam-mode', component: ExamModeComponent },
       { path: 'live-mode', component: LiveModeComponent },
       { path: 'study-planner/:plan_id', component: StudyPlannerStudentComponent },
-      { path: 'exam-prepmode', component: ExamPrepmodeComponent },
       { path: 'study-planner/test/:day/:plan_id', component: TestsPageComponent },
     ],
   },
