bc1957b1a44cbebda79b6d49218071c10d16c235
mohanchaitanya <mohanchaitanya.k@sparshcom.net>
2022-02-06 11:26:28 +0530

    for designs - mohan

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -68,9 +68,9 @@ export const environment = {
 
   //apiUrl: 'http://192.10.250.150:8000/api/',
   //apiUrl: 'http://127.0.0.1:8000/api/',
-  // apiUrl: 'https://dev.medvizz3d.com/web-api/public/api/',
-  apiUrl: 'https://apiqa.proceum.com/api/',
-
+   apiUrl: 'https://dev.medvizz3d.com/web-api/public/api/',
+  // apiUrl: 'https://apiqa.proceum.com/api/',
+  
   //APP_BASE_URL: 'http://localhost:4200/',
    APP_BASE_URL: 'http://192.10.250.150:4200/',
 
