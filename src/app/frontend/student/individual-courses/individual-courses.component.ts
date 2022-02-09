f121fffbf35997a2153afb5ff68cd3d391930721
mohanchaitanya <mohanchaitanya.k@sparshcom.net>
2022-02-09 11:38:50 +0530

    for designs - mohan

---
+++ src/app/frontend/student/individual-courses/individual-courses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-individual-courses',
+  templateUrl: './individual-courses.component.html',
+  styleUrls: ['./individual-courses.component.scss']
+})
+export class IndividualCoursesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
