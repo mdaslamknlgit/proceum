025946864b205422d717b64c838ad586ae455b46
Sandeep Kumar Sekharmahanthi <Sandeep.kumar@sparshcom.net>
2022-02-10 11:26:47 +0530

    free content and contactus bug fixes

--- src/app/frontend/student/my-account/my-account.component.ts
+++ src/app/frontend/student/my-account/my-account.component.ts
@@ -33,7 +33,9 @@ export class MyAccountComponent implements OnInit {
   profile: Profile = {
     first_name: '',
     last_name: '',
+    phone: '',
     email: '',
+    uuid: '',
     profile_pic: '',
     current_password: '',
     new_password: '',
@@ -166,7 +168,9 @@ export class MyAccountComponent implements OnInit {
       this.profile = {
         first_name: res['data'].first_name,
         last_name: res['data'].last_name,
+        phone: res['data'].phone,
         email: res['data'].email,
+        uuid: res['data'].uuid,
         profile_pic: '',
         current_password: '',
         new_password: '',
@@ -307,7 +311,9 @@ export interface Response {
 export interface Profile {
   first_name: string;
   last_name: string;
+  phone: string;
   email: string;
+  uuid: string;
   current_password: any;
   new_password: any;
   confirm_pwd: any;
