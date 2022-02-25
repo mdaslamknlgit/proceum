e66d6b53e7176a9b39d00c27dfaf2c3795c675ac
phanindra.ch <phanindra.ch@sparshcom.net>
2022-02-25 15:58:34 +0530

    Issues Fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  file_upload_size: 2040,//in kb
+  file_upload_size: 512000,//in kb
   page_size: 10,
   page_size_options: [10, 20, 50, 100],
   liteEditorConfig: {
